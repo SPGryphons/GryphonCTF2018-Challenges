35cc9057037a5cf4132dc98a5c955af3012ef7dc
David Zhu <zhy.david@gmail.com>
2018-09-17 00:56:32 +0800

    Web: Vaccinated (#4)
    * Web: Vaccinated

---
+++ challenges/web/Vaccinated/service/expressapp/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+let userSchema = mongoose.Schema({
+	username:{
+		type: String,
+		required: true
+	},
+	password:{
+		type: String,
+		required: true
+	}
+});
+
+let User = module.exports = mongoose.model('Users', userSchema);
---
+++ challenges/web/Vaccinated/service/mongo/data.js
@@ -0,0 +1,2 @@
+db.createCollection('users');
+db.users.insert({username:"Lorcan",password:"9Co8QoKWEGhoMmtSY@Bz#y89uuR0gb9UI*URe3"});
