35d0828dd39942257c1272c8f23a3cd497b15bcc
Noans <jonjontan610@hotmail.com>
2018-10-01 21:02:09 +0800

    Updated with allocated ports

--- challenges/web/Logmein/service/build.sh
+++ challenges/web/Logmein/service/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t logmein .
-docker run --restart always --memory 128M -d -p 18000:3000 --name web-logmein logmein
+docker run --restart always --memory 128M -d -p 18703:3000 --name web-logmein logmein
